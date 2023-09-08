915e8d2bea5d4b2d48f8e5346844f7bb121a6fb2
Amanda Carvalheiro Barbosa <56411960+amandacarvalheirobarbosa@users.noreply.github.com>
2023-09-08 09:06:58 -0300

    Update api.php

--- src/api/api.php
+++ src/api/api.php
@@ -1,7 +1,6 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  // Dados do formulário
   $Nome = $_POST["nome"];
   $Email = $_POST["email"];
   $CNPJ = $_POST["cnpj"];
@@ -18,11 +17,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $TipoServico = $_POST["tipoServico"];
   $Arquivos = $_FILES["arquivos"];
 
-  // Configurações de e-mail
-  $destinatario = "amanda.c@gvdsolucoes.com.br"; // Endereço de e-mail para onde enviar a mensagem
+  $destinatario = ""; // Endereço de e-mail para onde enviar a mensagem
   $assunto = "Mensagem do formulário de contato";
 
-  // Monta a mensagem
   $corpo_mensagem = "Nome: " . $Nome . "\n";
   $corpo_mensagem .= "E-mail: " . $Email . "\n";
   $corpo_mensagem .= "CNPJ: " . $CNPJ . "\n";
@@ -38,7 +35,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $corpo_mensagem .= "UF: " . $UF . "\n";
   $corpo_mensagem .= "Tipo Serviço: " . $TipoServico . "\n";
 
-  // Configurações do e-mail
   $boundary = md5(time());
   $headers = "From: $Email\r\n";
   $headers .= "MIME-Version: 1.0\r\n";
@@ -48,32 +44,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
   $message .= $corpo_mensagem . "\r\n";
 
-  /* // Processar uploads de arquivos
-  $anexos = array();
-
-  foreach ($_FILES["anexos"]["error"] as $key => $error) {
-    if ($error == UPLOAD_ERR_OK) {
-      $tmp_name = $_FILES["anexos"]["tmp_name"][$key];
-      $name = basename($_FILES["anexos"]["name"][$key]);
-      $anexos[] = array("name" => $name, "tmp_name" => $tmp_name);
-
-      // Especifique o caminho completo para a pasta onde deseja armazenar os anexos
-      //$caminho_destino = "./apoio/media/" . $name;
-      //move_uploaded_file($tmp_name, $caminho_destino); 
-    } else {
-      echo "Erro no upload dos arquivos: " . $error;
-    }
-  }  
-
-  foreach ($anexos as $anexo) {
-    $message .= "--$boundary\r\n";
-    $message .= "Content-Type: " . $anexo["type"] . ";  name=\"" . $anexo["name"] . "\"\r\n";
-    $message .= "Content-Transfer-Encoding: base64\r\n";
-    $message .= "Content-Disposition: attachment; filename=" . $anexo['name'] . "\r\n";
-    $message .= chunk_split(base64_encode(file_get_contents($anexo["tmp_name"]))) . "\r\n";
-  } */
-
-  // Processar uploads de arquivos e anexá-los ao e-mail
   foreach ($Arquivos["tmp_name"] as $key => $tmp_name) {
     $name = $Arquivos["name"][$key];
     $type = $Arquivos["type"][$key];
